5dfc790eedbae41c2f3b5c509336633752fc0911
Ricky Cook <mail@thatpanda.com>
2016-05-18 13:49:03 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+setup(name="MT Remote",
+      version="0.0.1",
+      description="MetaTrader remote interface",
+      author="Ricky Cook",
+      author_email="mt_remote@auto.thatpanda.com",
+      url="https://github.com/RickyCook/mt_remote",
+      py_modules=['mt_remote'],
+      install_requires=[str(ir.req) for ir in install_reqs],
+)
